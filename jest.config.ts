32aa6a10296bd48c0669b52c9cc41e13c8455bcf
Skryabin Aleksey <skryabin.aleksey99@gmail.com>
2026-03-09 09:05:31 +0500

    feature: update formatter config

--- jest.config.ts
+++ jest.config.ts
@@ -60,9 +60,7 @@ const config: JestConfigWithTsJest = {
 		},
 	},
 	// Трансформация ESM модулей из @makebelieve21213-packages
-	transformIgnorePatterns: [
-		"node_modules/(?!(@makebelieve21213-packages)/)",
-	],
+	transformIgnorePatterns: ["node_modules/(?!(@makebelieve21213-packages)/)"],
 };
 
 export default config;
